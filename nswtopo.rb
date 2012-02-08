a78f33167ad8c391a435786e812b8de7ac8381c4
Matthew Hollingworth <mdholling@gmail.com>
2012-02-08 12:04:42 +1100

    refactored kml file generation

--- nswtopo.rb
+++ nswtopo.rb
@@ -1241,7 +1241,15 @@ glow:
   end
   
   module KMZ
-    TILE_SIZE = 9
+    TILE_SIZE = 9 # 512 x 512 tiles
+    
+    def self.style
+      lambda do |style|
+        style.add_element("ListStyle", "id" => "hideChildren") do |list_style|
+          list_style.add_element("listItemType") { |type| type.text = "checkHideChildren" }
+        end
+      end
+    end
     
     def self.lat_lon_box(bounds)
       lambda do |box|
@@ -1261,15 +1269,23 @@ glow:
       end
     end
     
+    def self.network_link(bounds, path)
+      lambda do |network|
+        network.add_element("Region", &region(bounds))
+        network.add_element("Link") do |link|
+          link.add_element("href") { |href| href.text = path }
+          link.add_element("viewRefreshMode") { |mode| mode.text = "onRegion" }
+          link.add_element("viewFormat")
+        end
+      end
+    end
+    
     def self.build(map_name, bounds, projection, scaling, image_path, kmz_path)
       wgs84_bounds = Bounds.transform(projection, WGS84, bounds)
       degrees_per_pixel = 180.0 * scaling.metres_per_pixel / Math::PI / EARTH_RADIUS
       dimensions = wgs84_bounds.map { |bound| bound.reverse.inject(:-) / degrees_per_pixel }
       max_zoom = Math::log2(dimensions.max).ceil - TILE_SIZE
       Dir.mktmpdir do |temp_dir|
-        blank_tile_path = File.join(temp_dir, "blank.tif")
-        %x[convert -size #{1 << TILE_SIZE}x#{1 << TILE_SIZE} canvas:none -type TrueColor -depth 8 "#{blank_tile_path}"]
-        
         topleft = [ wgs84_bounds.first.min, wgs84_bounds.last.max ]
         pyramid = 0.upto(max_zoom).map do |zoom|
           resolution = degrees_per_pixel * 2**(max_zoom - zoom)
@@ -1304,19 +1320,15 @@ glow:
             
             topleft = [ tile_bounds.first.min, tile_bounds.last.max ]
             WorldFile.write(topleft, resolution, 0, tile_tfw_path)
-            FileUtils.cp(blank_tile_path, tile_tif_path)
-            %x[gdalwarp -s_srs "#{projection}" -t_srs "#{WGS84}" -r bilinear "#{image_path}" "#{tile_tif_path}"]
+            %x[convert -size #{1 << TILE_SIZE}x#{1 << TILE_SIZE} canvas:none -type TrueColorMatte -depth 8 "#{tile_tif_path}"]
+            %x[gdalwarp -s_srs "#{projection}" -t_srs "#{WGS84}" -r bilinear -dstalpha "#{image_path}" "#{tile_tif_path}"]
             %x[convert "#{tile_tif_path}" -quiet "#{tile_png_path}"]
             
             xml = REXML::Document.new
             xml << REXML::XMLDecl.new(1.0, "UTF-8")
             xml.add_element("kml", "xmlns" => "http://earth.google.com/kml/2.1") do |kml|
               kml.add_element("Document") do |document|
-                document.add_element("Style") do |style|
-                  style.add_element("ListStyle", "id" => "hideChildren") do |list_style|
-                    list_style.add_element("listItemType") { |type| type.text = "checkHideChildren" }
-                  end
-                end
+                document.add_element("Style", &style)
                 document.add_element("Region", &region(tile_bounds))
                 document.add_element("GroundOverlay") do |overlay|
                   overlay.add_element("drawOrder") { |draw_order| draw_order.text = zoom }
@@ -1325,21 +1337,13 @@ glow:
                   end
                   overlay.add_element("LatLonBox", &lat_lon_box(tile_bounds))
                 end
-                unless zoom == max_zoom
+                if zoom < max_zoom
                   indices.map do |index|
                     [ 2 * index, 2 * index + 1 ]
                   end.inject(:product).select do |subindices|
                     pyramid[zoom + 1][subindices]
-                  end.map do |subindices|
-                    subbounds = pyramid[zoom + 1][subindices]
-                    document.add_element("NetworkLink") do |network|
-                      network.add_element("Region", &region(subbounds))
-                      network.add_element("Link") do |link|
-                        link.add_element("href") { |href| href.text = "../../#{[ zoom+1, *subindices ].join ?/}.kml" }
-                        link.add_element("viewRefreshMode") { |mode| mode.text = "onRegion" }
-                        link.add_element("viewFormat")
-                      end
-                    end
+                  end.each do |subindices|
+                    document.add_element("NetworkLink", &network_link(pyramid[zoom + 1][subindices], "../../#{[ zoom+1, *subindices ].join ?/}.kml"))
                   end
                 end
               end
@@ -1353,22 +1357,10 @@ glow:
         xml.add_element("kml", "xmlns" => "http://earth.google.com/kml/2.1") do |kml|
           kml.add_element("Document") do |document|
             document.add_element("Name") { |name| name.text = map_name }
-            document.add_element("Style") do |style|
-              style.add_element("ListStyle", "id" => "hideChildren") do |list_style|
-                list_style.add_element("listItemType") { |type| type.text = "checkHideChildren" }
-              end
-            end
-            document.add_element("NetworkLink") do |network|
-              network.add_element("Region", &region(pyramid[0][[0,0]]))
-              network.add_element("Link") do |link|
-                link.add_element("href") { |href| href.text = "0/0/0.kml" }
-                link.add_element("viewRefreshMode") { |mode| mode.text = "onRegion" }
-                link.add_element("viewFormat")
-              end
-            end
+            document.add_element("Style", &style)
+            document.add_element("NetworkLink", &network_link(pyramid[0][[0,0]], "0/0/0.kml"))
           end
         end
-        
         kml_path = File.join(kmz_dir, "doc.kml")
         File.open(kml_path, "w") { |file| file << xml }
         

c3662061240cb3be110135ccaf54f08c623d788e
Matthew Hollingworth <mdholling@gmail.com>
2015-01-22 20:20:08 +1100

    split polygons during clipping

--- nswtopo.rb
+++ nswtopo.rb
@@ -315,11 +315,10 @@ class Array
     ring.map(&:difference).map(&:perp)
   end
   
-  def clip(hull, closed = true)
+  def clip_lines(hull)
     [ hull, hull.perps ].transpose.inject(self) do |result, (vertex, perp)|
       result.inject([]) do |clipped, points|
-        segments = closed ? points.ring : [ *points, points.last ].segments
-        clipped + segments.inject([[]]) do |lines, segment|
+        clipped + [ *points, points.last ].segments.inject([[]]) do |lines, segment|
           inside = segment.map { |point| point.minus(vertex).dot(perp) <= 0 }
           case
           when inside.all?
@@ -328,7 +327,7 @@ class Array
             lines.last << segment[0]
             lines.last << segment.along(vertex.minus(segment[0]).dot(perp) / segment.difference.dot(perp))
           when inside[1]
-            lines << [ ] unless closed
+            lines << [ ]
             lines.last << segment.along(vertex.minus(segment[0]).dot(perp) / segment.difference.dot(perp))
           end
           lines
@@ -337,8 +336,58 @@ class Array
     end.select(&:many?)
   end
   
-  def clip!(*args)
-    replace clip(*args)
+  def clip_lines!(hull)
+    replace clip_lines(hull)
+  end
+  
+  def clip_polys(hull)
+    [ hull, hull.perps ].transpose.inject(self) do |polygons, (vertex, perp)|
+      polygons.inject([]) do |clipped, polygon|
+        insides = polygon.map { |point| point.minus(vertex).dot(perp) <= 0 }
+        case
+        when insides.all? then clipped << polygon
+        when insides.none?
+        else
+          outgoing = insides.ring.map.with_index.select { |inside, index| inside[0] && !inside[1] }.map(&:last)
+           ingoing = insides.ring.map.with_index.select { |inside, index| !inside[0] && inside[1] }.map(&:last)
+          pairs = [ outgoing, ingoing ].map do |indices|
+            polygon.ring.map.with_index.to_a.values_at(*indices).map do |segment, index|
+              [ segment.along(vertex.minus(segment[0]).dot(perp).to_f / segment.difference.dot(perp)), index ]
+            end.sort_by do |intersection, index|
+              [ vertex.minus(intersection).dot(perp.perp), index ]
+            end
+          end.transpose
+          clipped << []
+          while pairs.any?
+            index ||= pairs[0][1][1]
+            start ||= pairs[0][0][1]
+            pair = pairs.min_by do |pair|
+              intersections, indices = pair.transpose
+              (indices[0] - index) % polygon.length
+            end
+            pairs.delete pair
+            intersections, indices = pair.transpose
+            while (indices[0] - index) % polygon.length > 0
+              index += 1
+              index %= polygon.length
+              clipped.last << polygon[index]
+            end
+            clipped.last << intersections[0] << intersections[1]
+            if index == start
+              clipped << []
+              index = start = nil
+            else
+              index = indices[1]
+            end
+          end
+        end
+        clipped.select(&:any?)
+      end
+    end
+  end
+  
+  def clip_polys!(hull)
+    replace clip_polys(hull)
   end
   
   def surrounds?(points)
@@ -1618,7 +1667,7 @@ IWH,Map Image Width/Height,#{dimensions.join ?,}
                 feature["geometry"][dimension == 2 ? "rings" : "paths"].map do |points|
                   reproject ? map.reproject_from(projection, points) : points
                 end.tap do |lines|
-                  lines.clip! map.coord_corners(1.0), dimension == 2
+                  dimension == 1 ? lines.clip_lines!(map.coord_corners(1.0)) : lines.clip_polys!(map.coord_corners(1.0))
                 end
               end
               attributes = feature["attributes"]
@@ -1915,7 +1964,7 @@ IWH,Map Image Width/Height,#{dimensions.join ?,}
           points.along (start + n) * step / points.distance
         end.unshift(points.first).push(points.last)
       end.tap do |lines|
-        lines.clip! map.mm_corners, false
+        lines.clip_lines! map.mm_corners
       end.map do |points|
         points.to_path_data MM_DECIMAL_DIGITS
       end.each do |d|
@@ -1971,7 +2020,7 @@ IWH,Map Image Width/Height,#{dimensions.join ?,}
         lines.inject(&:+).map do |line|
           map.coords_to_mm map.reproject_from(utm, line)
         end.tap do |lines|
-          lines.clip! map.mm_corners, false
+          lines.clip_lines! map.mm_corners
         end.each do |points|
           group.add_element("path", "d" => points.to_path_data(MM_DECIMAL_DIGITS))
         end
@@ -1988,7 +2037,7 @@ IWH,Map Image Width/Height,#{dimensions.join ?,}
         lines.map.with_index do |lines, index|
           lines.select(&:first).map do |line|
             coords = map.reproject_from(utm, [ line.first, line.last ])
-            [ line[0][index], [ coords ].clip(map.coord_corners(-5), false)[0] ]
+            [ line[0][index], [ coords ].clip_lines(map.coord_corners(-5))[0] ]
           end.select(&:last).map do |coord, points|
             labels_percents = [ [ "%d" % (coord / 100000), 80 ], [ "%02d" % ((coord / 1000) % 100), 100 ] ]
             labels_percents << [ "%03d" % (coord % 1000), 80 ] unless interval % 1000 == 0

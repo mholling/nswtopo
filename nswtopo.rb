2562ababa491306341a19b126fe2cf1f62a9d45c
Matthew Hollingworth <mdholling@gmail.com>
2015-07-06 16:54:28 +1000

    allow DEM files in non-wgs84 projections

--- nswtopo.rb
+++ nswtopo.rb
@@ -2102,7 +2102,12 @@ IWH,Map Image Width/Height,#{dimensions.join ?,}
     
     def get_raster(map, dimensions, resolution, temp_dir)
       src_path = temp_dir + "dem.txt"
-      dem_path = temp_dir + "dem.vrt"
+      vrt_path = temp_dir + "dem.vrt"
+      dem_path = temp_dir + "dem.tif"
+      
+      bounds = map.bounds.map do |lower, upper|
+        [ lower - 10.0 * resolution, upper + 10.0 * resolution ]
+      end
       
       if params["path"]
         [ *params["path"] ].map do |path|
@@ -2112,7 +2117,8 @@ IWH,Map Image Width/Height,#{dimensions.join ?,}
         end
       else
         base_uri = URI.parse "http://www.ga.gov.au/gisimg/rest/services/topography/dem_s_1s/ImageServer/"
-        base_query = { "f" => "json", "geometry" => map.wgs84_bounds.map(&:sort).transpose.flatten.plus([ -0.001, -0.001, 0.001, 0.001 ]).join(?,) }
+        wgs84_bounds = map.projection.transform_bounds_to Projection.wgs84, bounds
+        base_query = { "f" => "json", "geometry" => wgs84_bounds.map(&:sort).transpose.flatten.join(?,) }
         query = base_query.merge("returnIdsOnly" => true, "where" => "category = 1").to_query
         raster_ids = ArcGIS.get_json(base_uri + "query?#{query}").fetch("objectIds")
         query = base_query.merge("rasterIDs" => raster_ids.join(?,), "format" => "TIFF").to_query
@@ -2130,7 +2136,15 @@ IWH,Map Image Width/Height,#{dimensions.join ?,}
       end.join(?\n).tap do |path_list|
         src_path.write path_list
       end
-      %x[gdalbuildvrt -input_file_list "#{src_path}" "#{dem_path}"]
+      %x[gdalbuildvrt -input_file_list "#{src_path}" "#{vrt_path}"]
+      
+      dem_bounds = map.projection.transform_bounds_to Projection.new(vrt_path), bounds
+      ulx, lrx, lry, uly = dem_bounds.flatten
+      %x[gdal_translate -projwin #{ulx} #{uly} #{lrx} #{lry} "#{vrt_path}" "#{dem_path}"]
+      
+      scale = bounds.zip(dem_bounds).last.map do |bound|
+        bound.inject(&:-)
+      end.inject(&:/)
       
       temp_dir.join(path.basename).tap do |tif_path|
         relief_path = temp_dir + "#{name}-uncropped.tif"
@@ -2138,10 +2152,10 @@ IWH,Map Image Width/Height,#{dimensions.join ?,}
         map.write_world_file tfw_path, resolution
         density = 0.01 * map.scale / resolution
         altitude, azimuth, exaggeration = params.values_at("altitude", "azimuth", "exaggeration")
-        %x[gdaldem hillshade -compute_edges -s 111120 -alt #{altitude} -z #{exaggeration} -az #{azimuth} "#{dem_path}" "#{relief_path}" -q]
+        %x[gdaldem hillshade -compute_edges -s #{scale} -alt #{altitude} -z #{exaggeration} -az #{azimuth} "#{dem_path}" "#{relief_path}" -q]
         raise BadLayerError.new("invalid elevation data") unless $?.success?
         %x[convert -size #{dimensions.join ?x} -units PixelsPerCentimeter -density #{density} canvas:none -type GrayscaleMatte -depth 8 "#{tif_path}"]
-        %x[gdalwarp -s_srs "#{Projection.wgs84}" -t_srs "#{map.projection}" -r bilinear -srcnodata 0 -dstalpha "#{relief_path}" "#{tif_path}"]
+        %x[gdalwarp -t_srs "#{map.projection}" -r bilinear -srcnodata 0 -dstalpha "#{relief_path}" "#{tif_path}"]
         filters = []
         (params["median"].to_f / resolution).round.tap do |pixels|
           filters << "-statistic median #{2 * pixels + 1}" if pixels > 0

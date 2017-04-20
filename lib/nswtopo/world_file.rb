cf8f57519b2719cfb0c5e34334e565406530b8d7
Matthew Hollingworth <mdholling@gmail.com>
2017-04-21 08:32:04 +1000

    refactor world file method

--- lib/nswtopo/world_file.rb
+++ lib/nswtopo/world_file.rb
@@ -1,13 +1,16 @@
 module NSWTopo
   module WorldFile
-    def self.write(topleft, resolution, angle, path)
+    def self.affine_transform(top_left, resolution, angle)
+      sin, cos = Math::sin(angle * Math::PI / 180.0), Math::cos(angle * Math::PI / 180.0)
+      [ [ resolution * cos,  resolution * sin, top_left[0] ],
+        [ resolution * sin, -resolution * cos, top_left[1] ] ]
+    end
+    
+    def self.write(top_left, resolution, angle, path)
+      (r00, r01, x), (r10, r11, y) = affine_transform(top_left, resolution, angle)
       path.open("w") do |file|
-        file.puts  resolution * Math::cos(angle * Math::PI / 180.0)
-        file.puts  resolution * Math::sin(angle * Math::PI / 180.0)
-        file.puts  resolution * Math::sin(angle * Math::PI / 180.0)
-        file.puts -resolution * Math::cos(angle * Math::PI / 180.0)
-        file.puts topleft.first + 0.5 * resolution
-        file.puts topleft.last - 0.5 * resolution
+        file.puts r00, r01, r10, r11
+        file.puts x + 0.5 * resolution, y - 0.5 * resolution
       end
     end
   end

mholling/nswtopo

commit fd52bc92bfb040ed6a3cf988fd687c1546a15400

carry perp vector instead of search vector to avoid numerical issue in Gilbert-Johnson-Keerthi

lib/geometry/overlap.rb
@@ -1,34 +1,34 @@
 module Overlap
   def separated_by?(buffer)
     simplex = [ map(&:first).inject(&:minus) ]
-    search = simplex[0].negate
+    perp = simplex[0].perp
     loop do
-      return false unless simplex[0].dot(search.normalised).abs > buffer
-      max = self[0].max_by { |point| point.dot search }
-      min = self[1].min_by { |point| point.dot search }
+      return false unless simplex[0].cross(perp.normalised).abs > buffer
+      max = self[0].max_by { |point| point.cross perp }
+      min = self[1].min_by { |point| point.cross perp }
       support = max.minus min
-      return true unless simplex[0].minus(support).dot(search) < 0
+      return true unless simplex[0].minus(support).cross(perp) < 0
       rays = simplex.map { |point| point.minus support }
       case simplex.length
       when 1
         case
         when rays[0].dot(support) > 0
-          simplex, search = [ support ], support.negate
+          simplex, perp = [ support ], support.perp
         when rays[0].cross(support) < 0
-          simplex, search = [ support, *simplex ], rays[0].perp
+          simplex, perp = [ support, *simplex ], rays[0].negate
         else
-          simplex, search = [ *simplex, support ], rays[0].perp.negate
+          simplex, perp = [ *simplex, support ], rays[0]
         end
       when 2
         case
-        when rays[0].cross(support) > 0 && rays[0].perp.cross(support) > 0
-          simplex, search = [ simplex[0], support ], rays[0].perp.negate
-        when rays[1].cross(support) < 0 && rays[1].perp.cross(support) > 0
-          simplex, search = [ support, simplex[1] ], rays[1].perp
+        when rays[0].cross(support) > 0 && rays[0].dot(support) < 0
+          simplex, perp = [ simplex[0], support ], rays[0]
+        when rays[1].cross(support) < 0 && rays[1].dot(support) < 0
+          simplex, perp = [ support, simplex[1] ], rays[1].negate
         when rays[0].cross(support) <= 0 && rays[1].cross(support) >= 0
           return false
         else
-          simplex, search = [ support ], support.negate
+          simplex, perp = [ support ], support.perp
         end
       end
     end

commit 79e4851b1a7059b64ad11e49a1e817b2f64dcbfe

fix regression introduced by fd52bc9

lib/geometry/overlap.rb
@@ -9,10 +9,10 @@ module Overlap
       when simplex.inject(&:minus).dot(simplex[0]) < 0 then simplex[0].norm
       else simplex.inject(&:cross).abs / simplex.inject(&:minus).norm
       end > buffer
-      max = self[0].max_by { |point| point.cross perp }
-      min = self[1].min_by { |point| point.cross perp }
+      max = self[0].max_by { |point| perp.cross point }
+      min = self[1].min_by { |point| perp.cross point }
       support = max.minus min
-      return true unless simplex[0].minus(support).cross(perp) < 0
+      return true unless simplex[0].minus(support).cross(perp) > 0
       rays = simplex.map { |point| point.minus support }
       case simplex.length
       when 1
@@ -20,16 +20,16 @@ module Overlap
         when rays[0].dot(support) > 0
           simplex, perp = [ support ], support.perp
         when rays[0].cross(support) < 0
-          simplex, perp = [ support, *simplex ], rays[0].negate
+          simplex, perp = [ support, *simplex ], rays[0]
         else
-          simplex, perp = [ *simplex, support ], rays[0]
+          simplex, perp = [ *simplex, support ], rays[0].negate
         end
       when 2
         case
         when rays[0].cross(support) > 0 && rays[0].dot(support) < 0
-          simplex, perp = [ simplex[0], support ], rays[0]
+          simplex, perp = [ simplex[0], support ], rays[0].negate
         when rays[1].cross(support) < 0 && rays[1].dot(support) < 0
-          simplex, perp = [ support, simplex[1] ], rays[1].negate
+          simplex, perp = [ support, simplex[1] ], rays[1]
         when rays[0].cross(support) <= 0 && rays[1].cross(support) >= 0
           return false
         else
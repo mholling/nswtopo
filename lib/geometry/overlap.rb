79e4851b1a7059b64ad11e49a1e817b2f64dcbfe
Matthew Hollingworth <mdholling@gmail.com>
2017-05-27 18:56:27 +1000

    fix regression introduced by fd52bc9

--- lib/geometry/overlap.rb
+++ lib/geometry/overlap.rb
@@ -9,10 +9,10 @@ module Overlap
       when simplex.inject(&:minus).dot(simplex[0]) < 0 then simplex[0].norm
       else simplex.inject(&:cross).abs / simplex.inject(&:minus).norm
       end > buffer
-      max = self[0].max_by { |point| point.cross perp }
-      min = self[1].min_by { |point| point.cross perp }
+      max = self[0].max_by { |point| perp.cross point }
+      min = self[1].min_by { |point| perp.cross point }
       support = max.minus min
-      return true unless simplex[0].minus(support).cross(perp) < 0
+      return true unless simplex[0].minus(support).cross(perp) > 0
       rays = simplex.map { |point| point.minus support }
       case simplex.length
       when 1
@@ -20,16 +20,16 @@ module Overlap
         when rays[0].dot(support) > 0
           simplex, perp = [ support ], support.perp
         when rays[0].cross(support) < 0
-          simplex, perp = [ support, *simplex ], rays[0].negate
+          simplex, perp = [ support, *simplex ], rays[0]
         else
-          simplex, perp = [ *simplex, support ], rays[0]
+          simplex, perp = [ *simplex, support ], rays[0].negate
         end
       when 2
         case
         when rays[0].cross(support) > 0 && rays[0].dot(support) < 0
-          simplex, perp = [ simplex[0], support ], rays[0]
+          simplex, perp = [ simplex[0], support ], rays[0].negate
         when rays[1].cross(support) < 0 && rays[1].dot(support) < 0
-          simplex, perp = [ support, simplex[1] ], rays[1].negate
+          simplex, perp = [ support, simplex[1] ], rays[1]
         when rays[0].cross(support) <= 0 && rays[1].cross(support) >= 0
           return false
         else

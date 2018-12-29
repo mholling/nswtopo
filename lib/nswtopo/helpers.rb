a90284f1415ce03bf53597044ad793128f4e563f
Matthew Hollingworth <mdholling@gmail.com>
2018-12-29 16:54:00 +1100

    rearrange some files

--- lib/nswtopo/helpers.rb
+++ lib/nswtopo/helpers.rb
@@ -1,5 +1,4 @@
-require_relative 'helpers/http'
-require_relative 'helpers/wfs'
 require_relative 'helpers/dither'
 require_relative 'helpers/os'
 require_relative 'helpers/colour'
+require_relative 'helpers/font'

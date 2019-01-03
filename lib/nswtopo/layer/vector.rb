7214c355ddc59e7c4158ba55964bb12cb2034511
Matthew Hollingworth <mdholling@gmail.com>
2019-01-03 12:10:34 +1100

    add back code for SVG rendering of vector layers

--- lib/nswtopo/layer/vector.rb
+++ lib/nswtopo/layer/vector.rb
@@ -1,5 +1,8 @@
 module NSWTopo
   module Vector
+    SVG_ATTRIBUTES = %w[fill-opacity fill font-family font-size font-style font-variant font-weight letter-spacing opacity stroke-dasharray stroke-dashoffset stroke-linecap stroke-linejoin stroke-miterlimit stroke-opacity stroke-width stroke text-decoration visibility word-spacing]
+    SCALABLE_ATTRIBUTES = %w[word-spacing letter-spacing stroke-width]
+    SHIELD_X, SHIELD_Y = 1.0, 0.5
     MARGIN = { mm: 1.0 }
 
     def create
@@ -15,6 +18,12 @@ module NSWTopo
 
     def features
       GeoJSON::Collection.load @map.read(filename)
+    end
+
+    def drawing_features
+      features.explode.reject do |feature|
+        feature.properties["nodraw"]
+      end
     end
 
     extend Forwardable
@@ -49,199 +58,181 @@ module NSWTopo
       end
     end
 
+    def params_for(categories)
+      params.select do |command, args|
+        [ *command ].any? do |selector|
+          selector.to_s.split.to_set <= categories
+        end
+      end.values.inject(params, &:merge)
+    end
+
+    def fences
+      @fences ||= []
+    end
+
     def render(group, defs)
       to_mm = @map.method(:coords_to_mm)
-      features.explode.each do |feature|
-        case feature
-        when GeoJSON::Point
-          raise "TODO: not implemented"
-        when GeoJSON::LineString
-          d = svg_path_data feature.coordinates.map(&to_mm)
-          group.add_element "path", "d" => d, "fill" => "none", "stroke" => "black", "stroke-width" => 0.1
-        when GeoJSON::Polygon
-          d = feature.coordinates.map do |points|
-            svg_path_data points.map(&to_mm)
-          end.join(" Z ").concat(" Z")
-          # TODO: odd-even fill mode, etc
-          group.add_element "path", "d" => d, "fill" => "none", "stroke" => "black", "stroke-width" => 0.1
+
+      drawing_features.group_by do |feature|
+        feature.properties.fetch("categories", []).map(&:to_s).map(&:to_category).to_set
+      end.map do |categories, features|
+        dupes = params_for(categories)["dupe"]
+        [ *dupes ].map(&:to_s).map do |dupe|
+          [ categories | Set[dupe], [ name, *categories, "content" ].join(?.) ]
+        end.push [ categories, features ]
+      end.flatten(1).map do |categories, features|
+        ids = [ name, *categories ]
+        case features
+        when String
+          container = group.add_element "use", "class" => categories.to_a.join(?\s), "xlink:href" => "#%s" % features
+        when Array
+          container = group.add_element "g", "class" => categories.to_a.join(?\s)
+          content = container.add_element "g", "id" => [ *ids, "content" ].join(?.)
         end
+        container.add_attribute "id", ids.join(?.) if categories.any?
+
+        commands = params_for categories
+        font_size, bezier, section = commands.values_at "font-size", "bezier", "section"
+        SCALABLE_ATTRIBUTES.each do |name|
+          commands[name] = commands[name].to_i * font_size * 0.01 if /^\d+%$/ === commands[name]
+        end
+
+        features.each do |feature|
+          case feature
+          when GeoJSON::Point
+            symbol_id = [ *ids, "symbol"].join(?.)
+            # TODO: check rotation angle works!
+            transform = "translate(%f %f) rotate(%.2f)" % [ *feature.coordinates.yield_self(&to_mm), feature.properties.fetch("angle", 0) - @map.rotation ]
+            content.add_element "use", "transform" => transform, "xlink:href" => "#%s" % symbol_id
+
+          when GeoJSON::LineString
+            linestring = feature.coordinates.map(&to_mm)
+            (section ? linestring.in_sections(section) : [ linestring ]).each do |linestring|
+              content.add_element "path", "fill" => "none", "d" => svg_path_data(linestring, bezier: bezier)
+            end
+
+          when GeoJSON::Polygon
+            d = feature.coordinates.map do |ring|
+              svg_path_data ring.map(&to_mm), bezier: bezier
+            end.join(" Z ").concat(" Z")
+            # TODO: check fill-rule!!!
+            content.add_element "path", "fill-rule" => "nonzero", "d" => d
+
+          # # TODO: re-introduce when we bring back Label layer
+          # when REXML::Element
+          #   case feature.name
+          #   when "text", "textPath" then content << element
+          #   when "path" then defs << element
+          #   end
+          end
+        end if content
+
+        commands.each do |command, args|
+          next unless args
+          args = args.map(&:to_a).inject([], &:+) if Array === args && args.all?(Hash)
+
+          case command
+          when "blur"
+            filter_id = [ *ids, "blur" ].join(?.)
+            container.add_attribute "filter", "url(#%s)" % filter_id
+            defs.add_element("filter", "id" => filter_id).add_element "feGaussianBlur", "stdDeviation" => args, "in" => "SourceGraphic"
+
+          when "opacity"
+            if categories.none?
+              group.add_attribute "style", "opacity:#{args}"
+            else
+              container.add_attribute "opacity", args
+            end
+
+          when "symbol"
+            next unless content
+            symbol = defs.add_element "g", "id" => [ *ids, "symbol"].join(?.)
+            args.each &symbol.method(:add_element)
+
+          when "pattern"
+            width, height = Hash[args].values_at "width", "height"
+            pattern_id = [ *ids, "pattern"].join(?.)
+            pattern = defs.add_element "pattern", "id" => pattern_id, "patternUnits" => "userSpaceOnUse", "patternTransform" => "rotate(#{-@map.rotation})", "width" => width, "height" => height
+            args.each &pattern.method(:add_element)
+            container.add_attribute "fill", "url(#%s)" % pattern_id
+
+          when "symbolise"
+            next unless content
+            interval = Hash[args]["interval"]
+            symbol_ids = args.map.with_index do |(element, attributes), index|
+              symbol_id = [ *ids, "symbol", index ].join(?.).tap do |symbol_id|
+                defs.add_element("g", "id" => symbol_id).add_element(element, attributes)
+              end
+            end
+            lines_or_rings = features.grep(GeoJSON::LineString).map(&:coordinates)
+            lines_or_rings += features.grep(GeoJSON::Polygon).map(&:coordinates).flatten(1)
+            lines_or_rings.each do |points|
+              points.map(&to_mm).sample_at(interval, :angle).each do |point, angle|
+                transform = "translate(%f %f) rotate(%.2f)" % [ *point, 180.0 * angle / Math::PI ]
+                content.add_element "use", "transform" => transform, "xlink:href" => "#%s" % symbol_ids.sample
+              end
+            end
+
+          when "inpoint", "outpoint", "endpoint"
+            next unless content
+            symbol_id = [ *ids, command ].join(?.)
+            symbol = defs.add_element "g", "id" => symbol_id
+            args.each &symbol.method(:add_element)
+            features.grep(GeoJSON::LineString).map do |feature|
+              feature.coordinates.map(&to_mm)
+            end.each do |line|
+              case command
+              when "inpoint"  then [ line.first(2) ]
+              when "outpoint" then [ line.last(2).rotate ]
+              when "endpoint" then [ line.first(2), line.last(2).rotate ]
+              end.each do |segment|
+                transform = "translate(%f %f) rotate(%.2f)" % [ *segment.first, 180.0 * segment.difference.angle / Math::PI ]
+                container.add_element "use", "transform" => transform, "xlink:href" => "#%s" % symbol_id
+              end
+            end
+
+          # # TODO: reinstate, works with Label layer
+          # when "fence"
+          #   next unless content
+          #   buffer = 0.5 * (Numeric === args ? args : commands.fetch("stroke-width", 0))
+          #   features.each do |feature|
+          #     next if REXML::Element === features
+          #     fences << [ feature, buffer ]
+          #   end
+
+          # # TODO: reinstate, works with Label layer
+          # when "shield"
+          #   next unless content
+          #   content.elements.each("text") do |element|
+          #     text_length = element.elements["./ancestor-or-self::[@textLength]/@textLength"].value.to_f
+          #     group = REXML::Element.new("g")
+          #     width, height = text_length + SHIELD_X * font_size, (1 + SHIELD_Y) * font_size
+          #     group.add_element "rect", "x" => -0.5 * width, "y" => -0.5 * height, "width" => width, "height" => height, "rx" => font_size * 0.3, "ry" => font_size * 0.3, "stroke" => "none", "fill" => args
+          #     text_transform = element.attributes.get_attribute "transform"
+          #     text_transform.remove
+          #     group.attributes << text_transform
+          #     element.parent.elements << group
+          #     group << element
+          #   end
+
+          when *SVG_ATTRIBUTES
+            container.add_attribute command, args
+          end
+        end
+
+        next categories, features, container
+      end.tap do |categorised|
+        params.fetch("order", []).reverse.map(&:split).map(&:to_set).each do |filter|
+          categorised.select do |categories, features, container|
+            filter <= categories
+          end.reverse.each do |categories, features, container|
+            group.unshift container.remove
+          end
+        end
+      end
+
+      until group.elements.each(".//g[not(*)]", &:remove).empty? do
       end
     end
   end
 end
-
-# SHIELD_X, SHIELD_Y = 1.0, 0.5
-# SVG_PRESENTATION_ATTRIBUTES = %w[fill-opacity fill font-family font-size font-style font-variant font-weight letter-spacing opacity stroke-dasharray stroke-dashoffset stroke-linecap stroke-linejoin stroke-miterlimit stroke-opacity stroke-width stroke text-decoration visibility word-spacing]
-# SCALABLE_ATTRIBUTES = %w[word-spacing letter-spacing stroke-width]
-# attr_reader :name
-
-# def fences
-#   @fences ||= []
-# end
-
-# def render_svg(xml)
-
-#   features.group_by do |dimension, feature, categories, sublayer, *|
-#     sublayer
-#   end.each do |sublayer, features|
-#     next unless group = yield(sublayer)
-#     puts "  #{sublayer}" if sublayer
-#     group.add_attributes("transform" => transform) if transform
-#     sublayer_actions = params.merge params.fetch(sublayer, {})
-
-#     features.reject do |dimension, feature, *|
-#       feature.empty?
-#     end.group_by do |dimension, feature, categories, *|
-#       [ *categories ].map(&:to_s).map(&:to_category).to_set
-#     end.map do |categories, features|
-#       actions = sublayer_actions.select do |command, args|
-#         [ command ].flatten.any? do |filter|
-#           filter.to_s.split.to_set <= categories
-#         end
-#       end.values.inject(sublayer_actions, &:merge)
-#       [ *actions["dupe"] ].map do |dupe|
-#         [ categories + Set[dupe], [ name, *sublayer, *categories, "content" ].join(SEGMENT) ]
-#       end.push [ categories, features ]
-#     end.flatten(1).map do |categories, features|
-#       ids = [ name, *sublayer, *categories ]
-#       case features
-#       when String
-#         container = group.add_element "use", "xlink:href" => "##{features}", "class" => categories.map(&:to_s).join(?\s)
-#       when Array
-#         container = group.add_element "g", "class" => categories.map(&:to_s).join(?\s)
-#         content = container.add_element "g", "id" => [ *ids, "content" ].join(SEGMENT)
-#       end
-#       container.add_attribute "id", ids.join(SEGMENT) if categories.any?
-#       [ categories, features, container, content ]
-#     end.each do |categories, features, container, content|
-#       ids = [ name, *sublayer, *categories ]
-#       commands = sublayer_actions.select do |command, args|
-#         [ command ].flatten.any? do |filter|
-#           filter.to_s.split.to_set <= categories
-#         end
-#       end.values.inject(sublayer_actions, &:merge)
-#       font_size, bezier, section = commands.values_at "font-size", "bezier", "section"
-#       SCALABLE_ATTRIBUTES.each do |name|
-#         commands[name] = (commands[name].to_i * font_size * 0.01).round(5) if /^\d+%$/ === commands[name]
-#       end
-#       features.each do |dimension, feature, _, _, angle|
-#         case dimension
-#         when 0
-#           symbol_id = [ name, *sublayer, *categories, "symbol"].join(SEGMENT)
-#           feature.each do |x, y|
-#             content.add_element "use", "transform" => "translate(#{x} #{y}) rotate(#{angle || -CONFIG.map.rotation})", "xlink:href" => "##{symbol_id}"
-#           end
-#         when 1 then
-#           (section ? feature.in_sections(section).zip : [ feature ]).each do |feature|
-#             content.add_element "path", "fill" => "none", "d" => feature.to_path_data(MM_DECIMAL_DIGITS, false, bezier)
-#           end
-#         when 2 then
-#           content.add_element "path", "fill-rule" => "nonzero", "d" => feature.to_path_data(MM_DECIMAL_DIGITS, true, bezier)
-#         when nil
-#           feature.each do |element|
-#             case element.name
-#             when "text", "textPath" then content << element
-#             when "path" then defs << element
-#             end
-#           end
-#         end
-#       end if content
-#       commands.each do |command, args|
-#         next unless args
-#         args = args.map(&:to_a).inject([], &:+) if Array === args && args.all? { |arg| Hash === arg }
-#         case command
-#         when "blur"
-#           filter_id = [ *ids, "blur" ].join(SEGMENT)
-#           container.add_attribute "filter", "url(##{filter_id})"
-#           defs.add_element("filter", "id" => filter_id).add_element "feGaussianBlur", "stdDeviation" => args, "in" => "SourceGraphic"
-#         when "opacity"
-#           if sublayer_actions["opacity"] == args
-#             group.add_attribute "style", "opacity:#{args}"
-#           else
-#             container.add_attribute "opacity", args
-#           end
-#         when "symbol"
-#           next unless content
-#           symbol_id = [ *ids, "symbol"].join(SEGMENT)
-#           defs.add_element("g", "id" => symbol_id).tap do |symbol|
-#             args.each { |element, attributes| symbol.add_element element, attributes }
-#           end
-#         when "pattern"
-#           args = args.to_a
-#           width  = args.delete(args.find { |key, value| key == "width"  }).last
-#           height = args.delete(args.find { |key, value| key == "height" }).last
-#           pattern_id = [ *ids, "pattern"].join(SEGMENT)
-#           defs.add_element("pattern", "id" => pattern_id, "patternUnits" => "userSpaceOnUse", "patternTransform" => "rotate(#{-CONFIG.map.rotation})", "width" => width, "height" => height).tap do |pattern|
-#             args.each { |element, attributes| pattern.add_element element, attributes }
-#           end
-#           container.add_attribute "fill", "url(##{pattern_id})"
-#         when "symbolise"
-#           next unless content
-#           args = args.to_a
-#           interval = args.delete(args.find { |key, value| key == "interval" }).last
-#           symbol_ids = args.map.with_index do |(element, attributes), index|
-#             [ *ids, "symbol", index ].join(SEGMENT).tap do |symbol_id|
-#               defs.add_element("g", "id" => symbol_id).add_element(element, attributes)
-#             end
-#           end
-#           features.each do |dimension, feature, *|
-#             feature.sample_at(interval, :angle).each do |point, angle|
-#               translate = point.round(MM_DECIMAL_DIGITS).join ?\s
-#               rotate = (180.0 * angle / Math::PI).round(2)
-#               content.add_element "use", "transform" => "translate(#{translate}) rotate(#{rotate})", "xlink:href" => "##{symbol_ids.sample}"
-#             end if dimension && dimension != 0
-#           end
-#         when "inpoint", "outpoint", "endpoint"
-#           next unless content
-#           symbol_id = [ *ids, command ].join(SEGMENT)
-#           defs.add_element("g", "id" => symbol_id).tap do |symbol|
-#             args.each { |element, attributes| symbol.add_element element, attributes }
-#           end
-#           features.each do |dimension, feature, *|
-#             feature.each do |line|
-#               case command
-#               when "inpoint"  then [ line.first(2) ]
-#               when "outpoint" then [ line.last(2).rotate ]
-#               when "endpoint" then [ line.first(2), line.last(2).rotate ]
-#               end.each do |segment|
-#                 angle = 180.0 * segment.difference.angle / Math::PI
-#                 translate = segment[0].round(MM_DECIMAL_DIGITS).join ?\s
-#                 container.add_element "use", "transform" => "translate(#{translate}) rotate(#{angle.round 2})", "xlink:href" => "##{symbol_id}"
-#               end
-#             end if dimension == 1
-#           end
-#         when "fence"
-#           buffer = 0.5 * (Numeric === args ? args : commands.fetch("stroke-width", 0))
-#           features.each do |dimension, feature, *|
-#             fences << [ dimension, feature, buffer ]
-#           end if content
-#         when "shield"
-#           next unless content
-#           content.elements.each("text") do |element|
-#             text_length = element.elements["./ancestor-or-self::[@textLength]/@textLength"].value.to_f
-#             group = REXML::Element.new("g")
-#             width, height = text_length + SHIELD_X * font_size, (1 + SHIELD_Y) * font_size
-#             group.add_element "rect", "x" => -0.5 * width, "y" => -0.5 * height, "width" => width, "height" => height, "rx" => font_size * 0.3, "ry" => font_size * 0.3, "stroke" => "none", "fill" => args
-#             text_transform = element.attributes.get_attribute "transform"
-#             text_transform.remove
-#             group.attributes << text_transform
-#             element.parent.elements << group
-#             group << element
-#           end
-#         when *SVG_PRESENTATION_ATTRIBUTES
-#           container.add_attribute command, args
-#         end
-#       end
-#     end.tap do |categorised|
-#       sublayer_actions.fetch("order", []).reverse.map(&:split).map(&:to_set).each do |filter|
-#         categorised.select do |categories, features, container, content|
-#           filter <= categories
-#         end.reverse.each do |categories, features, container, content|
-#           group.unshift container.remove
-#         end
-#       end
-#     end
-#     until group.elements.each(".//g[not(*)]", &:remove).empty? do
-#     end
-#   end
-# end

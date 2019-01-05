7e88c93a7821dcb507962a7e36010f3df4fa09e1
Matthew Hollingworth <mdholling@gmail.com>
2019-01-05 20:16:56 +1100

    ditch psd output

--- lib/nswtopo/formats.rb
+++ lib/nswtopo/formats.rb
@@ -3,7 +3,6 @@ require_relative 'formats/kmz'
 require_relative 'formats/mbtiles'
 require_relative 'formats/zip'
 require_relative 'formats/pdf'
-# require_relative 'formats/psd'
 
 module NSWTopo
   module Formats

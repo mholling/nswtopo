1c844b2bcac57916d28ba28b63febf258e8a42e0
Matthew Hollingworth <mdholling@gmail.com>
2019-01-07 21:10:17 +1100

    no need for xml events namespace

--- lib/nswtopo/formats/svg.rb
+++ lib/nswtopo/formats/svg.rb
@@ -15,7 +15,6 @@ module NSWTopo
           "viewBox" => "0 0 #{width} #{height}",
           "xmlns" => "http://www.w3.org/2000/svg",
           "xmlns:xlink" => "http://www.w3.org/1999/xlink",
-          "xmlns:ev" => "http://www.w3.org/2001/xml-events", # TODO: necessary?
           "xmlns:sodipodi" => "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd",
           "xmlns:inkscape" => "http://www.inkscape.org/namespaces/inkscape",
         }

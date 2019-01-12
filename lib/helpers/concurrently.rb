6c8bc402cc046286c61e447cb1534cb2447b5907
Matthew Hollingworth <mdholling@gmail.com>
2019-01-12 21:05:47 +1100

    use Queue in Concurrently

--- lib/helpers/concurrently.rb
+++ lib/helpers/concurrently.rb
@@ -2,13 +2,15 @@ module Concurrently
   CORES = Etc.nprocessors rescue 1
 
   def concurrently(threads = CORES, &block)
-    inject [] do |threads, element|
-      while threads.length == CORES
-        sleep 1
-        threads, finished = threads.partition(&:alive?)
-        finished.each(&:join)
+    elements = Queue.new
+    threads.times.map do
+      Thread.new do
+        while element = elements.pop
+          block.call element
+        end
       end
-      threads << Thread.new(element, &block)
+    end.tap do
+      inject(elements, &:<<).close
     end.each(&:join)
     self
   end
@@ -16,7 +18,9 @@ module Concurrently
   def concurrent_groups(threads = CORES, &block)
     group_by.with_index do |item, index|
       index % threads
-    end.values.each.concurrently(&block)
+    end.values.map do |items|
+      Thread.new(items, &block)
+    end.each(&:join)
   end
 end
 

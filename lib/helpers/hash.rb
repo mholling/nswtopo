9c9985673fca65de53317be7c897f83a19463363
Matthew Hollingworth <mdholling@gmail.com>
2019-01-15 16:11:33 +1100

    oops

--- lib/helpers/hash.rb
+++ lib/helpers/hash.rb
@@ -1,13 +1,13 @@
 module HashHelpers
   def deep_merge(other)
     merge(other) do |key, old_value, new_value|
-      Hash === old_value ? Hash == new_value ? old_value.deep_merge(new_value) : new_value : new_value
+      Hash === old_value ? Hash === new_value ? old_value.deep_merge(new_value) : new_value : new_value
     end
   end
 
   def deep_merge!(other)
     merge!(other) do |key, old_value, new_value|
-      Hash === old_value ? Hash == new_value ? old_value.deep_merge!(new_value) : new_value : new_value
+      Hash === old_value ? Hash === new_value ? old_value.deep_merge!(new_value) : new_value : new_value
     end
   end
 end

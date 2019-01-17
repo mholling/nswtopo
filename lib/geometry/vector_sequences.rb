55a933ad5397e65493103480a44dd95513a04eac
Matthew Hollingworth <mdholling@gmail.com>
2019-01-17 13:15:30 +1100

    remove unneeded method

--- lib/geometry/vector_sequences.rb
+++ lib/geometry/vector_sequences.rb
@@ -2,15 +2,7 @@ module VectorSequences
   def sample_at(interval, **options)
     map do |sequence|
       sequence.periodically(interval, **options).to_a
-    end.inject([], &:+)
-  end
-
-  def sample_outwards(interval)
-    map(&:path_length).zip(self).map do |distance, line|
-      line.periodically(interval, along: true).map do |point, along|
-        [point, (2 * along - distance).abs - distance]
-      end
-    end.inject([], &:+).sort_by(&:last).map(&:first)
+    end.flatten(1)
   end
 end
 

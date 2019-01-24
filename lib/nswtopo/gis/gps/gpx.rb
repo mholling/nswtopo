68d8532bb2743e2ee7fa453517fe1bea79e88bcc
Matthew Hollingworth <mdholling@gmail.com>
2019-01-24 15:37:00 +1100

    GPX bugfixes

--- lib/nswtopo/gis/gps/gpx.rb
+++ lib/nswtopo/gis/gps/gpx.rb
@@ -2,7 +2,7 @@ module NSWTopo
   class GPS
     module GPX
       def collection
-        Collection.new.tap do |collection|
+        GeoJSON::Collection.new.tap do |collection|
           @xml.elements.each "/gpx/wpt" do |wpt|
             coords = %w[lon lat].map { |name| wpt.attributes[name].to_f }
             name = wpt.elements["name"]&.text
@@ -10,9 +10,9 @@ module NSWTopo
           end
           @xml.elements.each "/gpx/trk" do |trk|
             coords = trk.elements.collect("trkseg") do |trkseg|
-              trk.elements.collect("trkpt") { |trkpt| %w[lon lat].map { |name| trkpt.attributes[name].to_f } }
+              trkseg.elements.collect("trkpt") { |trkpt| %w[lon lat].map { |name| trkpt.attributes[name].to_f } }
             end
-            name = track.elements["./name"]&.text
+            name = trk.elements["name"]&.text
             collection.add_multilinestring coords, "name" => name
           end
         end

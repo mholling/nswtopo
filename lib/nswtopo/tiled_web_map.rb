701a86cb1f0935e986fac4e449f254982ad3d4b8
Matthew Hollingworth <mdholling@gmail.com>
2019-07-02 18:57:56 +1000

    add undocumented minimum-zoom parameter for tiled output formats

--- lib/nswtopo/tiled_web_map.rb
+++ lib/nswtopo/tiled_web_map.rb
@@ -2,14 +2,13 @@ module NSWTopo
   module TiledWebMap
     HALF, TILE_SIZE, DEFAULT_ZOOM = Math::PI * 6378137, 256, 16
 
-    def tiled_web_map(temp_dir, extension:, zoom: DEFAULT_ZOOM, **options, &block)
-      raise "invalid zoom outside 10-19 range: #{zoom}" unless (10..19) === zoom
-
+    def tiled_web_map(temp_dir, extension:, zoom: [DEFAULT_ZOOM], **options, &block)
       web_mercator_bounds = bounds(projection: Projection.new("EPSG:3857"))
       wgs84_bounds = bounds(projection: Projection.wgs84)
 
       png_path = nil
-      zoom.downto(0).inject([]) do |levels, zoom|
+      max_zoom, min_zoom = *zoom.sort.reverse
+      max_zoom.down_to(0).map do |zoom|
         indices, dimensions, topleft = web_mercator_bounds.map do |lower, upper|
           (2**zoom * (lower + HALF) / HALF / 2).floor ... (2**zoom * (upper + HALF) / HALF / 2).ceil
         end.map.with_index do |indices, axis|
@@ -17,20 +16,22 @@ module NSWTopo
         end.transpose
         tile_path = temp_dir.join("tile.#{zoom}.%09d.png").to_s
         resolution = 2 * HALF / TILE_SIZE / 2**zoom
-        levels << [resolution, indices, dimensions, topleft, tile_path, zoom]
-        break levels if indices.map(&:size).all? { |size| size < 3 }
-        levels
+        [resolution, dimensions, topleft, tile_path, indices, zoom]
+      end.select do |*, indices, zoom|
+        next true if zoom == max_zoom
+        next zoom >= min_zoom if min_zoom
+        indices.all? { |indices| indices.size < 3 } 
       end.tap do |(resolution, *), *|
         png_path = yield(resolution: resolution)
       end.tap do |levels|
         log_update "#{extension}: tiling for zoom levels %s" % levels.map(&:last).minmax.uniq.join(?-)
-      end.each.concurrently do |resolution, indices, dimensions, topleft, tile_path, zoom|
+      end.each.concurrently do |resolution, dimensions, topleft, tile_path, indices, zoom|
         tif_path, tfw_path = %w[tif tfw].map { |ext| temp_dir / "tile.#{zoom}.#{ext}" }
         WorldFile.write topleft, resolution, 0, tfw_path
         OS.convert "-size", dimensions.join(?x), "canvas:none", "-type", "TrueColorAlpha", "-depth", 8, tif_path
         OS.gdalwarp "-s_srs", @projection, "-t_srs", "EPSG:3857", "-r", "cubic", "-dstalpha", png_path, tif_path
         OS.convert tif_path, "-quiet", "+repage", "-crop", "#{TILE_SIZE}x#{TILE_SIZE}", tile_path
-      end.map do |resolution, indices, dimensions, topleft, tile_path, zoom|
+      end.map do |resolution, dimensions, topleft, tile_path, indices, zoom|
         cols, rows = indices.map(&:to_a)
         rows.reverse.product(cols).map.with_index do |(row, col), index|
           row ^= 2**zoom - 1 if extension == "gemf"

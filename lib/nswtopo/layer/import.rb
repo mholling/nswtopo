e5dd7f2cc9d2e5263def9cd9839b3aa7580f51f2
Matthew Hollingworth <mdholling@gmail.com>
2019-12-04 07:51:26 +1100

    respect relative paths in import layer files

--- lib/nswtopo/layer/import.rb
+++ lib/nswtopo/layer/import.rb
@@ -4,20 +4,19 @@ module NSWTopo
 
     def get_raster(temp_dir)
       crop_path = temp_dir / "crop.tif"
+      Dir.chdir(@source ? @source.parent : Pathname.pwd) do
+        json = OS.gdalinfo "-json", @path
+        palette = JSON.parse(json)["bands"].any? do |band|
+          "Palette" == band["colorInterpretation"]
+        end
 
-      json = begin
-        OS.gdalinfo "-json", @path
+        projection = Projection.new(@path)
+        args = ["-projwin", *@map.projwin(projection), @path, crop_path]
+        args += ["-expand", "rgba", *args] if palette
+        OS.gdal_translate *args
       rescue OS::Error
         raise "invalid raster file: #{@path}"
       end
-      palette = JSON.parse(json)["bands"].any? do |band|
-        "Palette" == band["colorInterpretation"]
-      end
-
-      projection = Projection.new(@path)
-      args = ["-projwin", *@map.projwin(projection), @path, crop_path]
-      args = ["-expand", "rgba", *args] if palette
-      OS.gdal_translate *args
 
       return Numeric === @resolution ? @resolution : @map.get_raster_resolution(crop_path), crop_path
     end

e6379fd606464d5821ed91f2d7b0adda94ff6e58
Matthew Hollingworth <mdholling@gmail.com>
2020-05-15 14:25:58 +1000

    give better progress indicators for zip format

--- lib/nswtopo/formats/zip.rb
+++ lib/nswtopo/formats/zip.rb
@@ -8,27 +8,35 @@ module NSWTopo
         top_left = bounding_box.coordinates[0][3]
 
         2.downto(0).map.with_index do |level, index|
-          [level, index, ppi.to_f / 2**index]
-        end.each.concurrently do |level, index, ppi|
-          dimensions, ppi, resolution = raster_dimensions_at ppi: ppi
-          img_path = index.zero? ? png_path : temp_dir / "#{name}.avenza.#{level}.png"
-          tile_path = temp_dir.join("#{name}.avenza.tile.#{level}.%09d.png").to_s
-
-          OS.convert png_path, "-filter", "Cubic", "-resize", "%ix%i!" % dimensions, img_path unless img_path.exist?
-          OS.convert img_path, "+repage", "-crop", "256x256", tile_path
-
-          dimensions.reverse.map do |dimension|
-            0.upto((dimension - 1) / 256).to_a
-          end.inject(&:product).each.with_index do |(y, x), n|
-            FileUtils.cp tile_path % n, tiles_dir / "#{level}x#{y}x#{x}.png"
-          end
+          [level, index, *raster_dimensions_at(ppi: ppi.to_f / 2**index)]
+        end.map do |level, index, dimensions, ppi, resolution|
           zip_dir.join("#{name}.ref").open("w") do |file|
             file.puts @projection.wkt_simple
             file.puts WorldFile.geotransform(top_left, resolution, -@rotation).flatten.join(?,)
             file << dimensions.join(?,)
           end if index == 1
+
+          img_path = index.zero? ? png_path : temp_dir / "#{name}.avenza.#{level}.png"
+          tile_path = temp_dir.join("#{name}.avenza.tile.#{level}.%09d.png").to_s
+          tile_paths = dimensions.reverse.map do |dimension|
+            0.upto((dimension - 1) / 256).to_a
+          end.inject(&:product).map.with_index do |(y, x), n|
+            [tile_path % n, tiles_dir / "#{level}x#{y}x#{x}.png"]
+          end
+          [dimensions, img_path, tile_path, tile_paths]
+        end.transpose.tap do |*, tile_paths|
+          log_update "zip: creating %i tiles" % tile_paths.sum(&:length)
+        end.transpose.each.concurrently do |dimensions, img_path, tile_path, tile_paths|
+          OS.convert png_path, "-filter", "Cubic", "-resize", "%ix%i!" % dimensions, img_path unless img_path.exist?
+          OS.convert img_path, "+repage", "-crop", "256x256", tile_path
+          tile_paths.each do |src, dst|
+            FileUtils.cp src, dst
+          end
         end
-        Pathname.glob(tiles_dir / "*.png").each.concurrent_groups do |tile_paths|
+
+        Pathname.glob(tiles_dir / "*.png").tap do |tile_paths|
+          log_update "zip: optimising %i tiles" % tile_paths.length
+        end.each.concurrent_groups do |tile_paths|
           dither *tile_paths
         end
 

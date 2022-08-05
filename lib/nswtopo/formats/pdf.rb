a100839aaa3e971337f5c1db355889ecdca4b848
Matthew Hollingworth <mdholling@gmail.com>
2022-08-05 11:21:44 +1000

    add fill-pattern workaround for Chrome PDF output

--- lib/nswtopo/formats/pdf.rb
+++ lib/nswtopo/formats/pdf.rb
@@ -7,11 +7,78 @@ module NSWTopo
         Dir.mktmppath do |temp_dir|
           svg_path = temp_dir / "pdf-map.svg"
           render_svg svg_path, background: background
-          xml = REXML::Document.new svg_path.read
-          style = "@media print { @page { margin: 0 0 -1mm 0; size: %s %s; } }"
-          svg = xml.elements["svg"]
-          svg.add_element("style").text = style % svg.attributes.values_at("width", "height")
-          svg_path.write xml
+
+          REXML::Document.new(svg_path.read).tap do |xml|
+            xml.elements["svg"].tap do |svg|
+              style = "@media print { @page { margin: 0 0 -1mm 0; size: %s %s; } }"
+              svg.add_element("style").text = style % svg.attributes.values_at("width", "height")
+            end
+
+            # replace fill pattern paint with manual pattern mosaic to work around Chrome PDF bug
+            xml.elements.each("svg//g[@fill]") do |group|
+              # find the pattern id, pattern element and content element
+              next unless /^url\(#(?<pattern_id>.*)\)$/ =~ group.attributes["fill"]
+              next unless pattern = group.elements["preceding-sibling::defs/pattern[@id='#{pattern_id}'][@width][@height]"]
+              next unless content = group.elements["g[contains(@id,'.content')]"]
+              content_id = content.attributes["id"]
+
+              # change pattern element to a group
+              pattern.attributes.delete "patternUnits"
+              pattern.name = "g"
+
+              # create a clip path to apply to the fill pattern mosaic
+              content_clip = REXML::Element.new "clipPath"
+              content_clip.add_attribute "id", "#{content_id}.clip"
+
+              # create a clip path to apply to pattern element
+              pattern_clip = REXML::Element.new "clipPath"
+              pattern_clip.add_attribute "id", "#{pattern_id}.clip"
+              pattern.add_attribute "clip-path", "url(##{pattern_id}.clip)"
+
+              # move content and clip paths into defs
+              pattern.previous_sibling = pattern_clip
+              pattern.next_sibling = content
+              content.next_sibling = content_clip
+
+              # create a container for the fill pattern mosaic
+              fill = REXML::Element.new "g"
+              fill.add_attribute "id", "#{content_id}.fill"
+              fill.add_attribute "clip-path", "url(##{content_id}.clip)"
+              group.previous_sibling = fill
+
+              # change group to reference the content, keeping stroke only
+              group.name = "use"
+              group.add_attribute "href", "##{content_id}"
+              group.add_attribute "fill", "none"
+              group.add_attribute "id", "#{content_id}.stroke"
+
+              # get pattern size
+              pattern_size = %w[width height].map do |name|
+                pattern.attributes[name].tap { pattern.attributes.delete name }
+              end.map(&:to_f)
+
+              # create pattern clip
+              pattern_size.each.with_object(0).inject(&:product).values_at(3,2,0,1).tap do |corners|
+                pattern_clip.add_element "path", "d" => %w[M L L L].zip(corners).push("Z").join(?\s)
+              end
+
+              # add paths to content clip, get content coverage area, and create fill pattern mosaic
+              content.elements.each_with_index("path[@d]") do |path, index|
+                path.add_attribute "id", "#{content_id}.#{index}"
+                content_clip.add_element "use", "href" => "##{content_id}.#{index}"
+              end.flat_map do |path|
+                path.attributes["d"].scan /(\d+(?:\.\d+)?) (\d+(?:\.\d+)?)/
+              end.transpose.map do |coords|
+                coords.map(&:to_f).minmax
+              end.zip(pattern_size).map do |(min, max), size|
+                (min...max).step(size).entries
+              end.inject(&:product).each do |x, y|
+                fill.add_element "use", "href" => "##{pattern_id}", "x" => x, "y" => y
+              end
+            end
+
+            svg_path.write xml
+          end
 
           FileUtils.rm pdf_path if pdf_path.exist?
           Config["chrome"].tap do |chrome_path|

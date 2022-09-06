9f443d8e88b9c6bce95b4f16a23de20aa982ebf0
Matthew Hollingworth <mdholling@gmail.com>
2022-09-06 20:38:17 +1000

    remove unnecessary error

--- lib/nswtopo/commands/add.rb
+++ lib/nswtopo/commands/add.rb
@@ -66,7 +66,6 @@ module NSWTopo
     end.tap do |layers|
       raise OptionParser::MissingArgument, "no layers specified" unless layers.any?
       unless layers.one?
-        raise OptionParser::InvalidArgument, "can't specify resolution when adding multiple layers" if options[:resolution]
         raise OptionParser::InvalidArgument, "can't specify opacity when adding multiple layers" if options[:opacity]
         raise OptionParser::InvalidArgument, "can't specify data path when adding multiple layers" if options[:path]
       end

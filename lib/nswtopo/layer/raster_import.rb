36482860da860da123b54b235e2d67dc336cd7f4
Matthew Hollingworth <mdholling@gmail.com>
2022-09-06 20:53:14 +1000

    handle relative paths better

--- lib/nswtopo/layer/raster_import.rb
+++ lib/nswtopo/layer/raster_import.rb
@@ -2,21 +2,21 @@ module NSWTopo
   module RasterImport
     def get_raster(temp_dir)
       crop_path = temp_dir / "crop.tif"
-      Dir.chdir(@source ? @source.parent : Pathname.pwd) do
-        json = OS.gdalinfo "-json", @path
-        palette = JSON.parse(json)["bands"].any? do |band|
-          "Palette" == band["colorInterpretation"]
-        end
+      @path = Pathname(@path).expand_path(@source ? @source.parent : Pathname.pwd)
 
-        projection = Projection.new(@path)
-        args = ["-projwin", *@map.projwin(projection), @path, crop_path]
-        args += ["-expand", "rgba", *args] if palette
-        OS.gdal_translate *args
-      rescue OS::Error
-        raise "invalid raster file: #{@path}"
+      json = OS.gdalinfo "-json", @path
+      palette = JSON.parse(json)["bands"].any? do |band|
+        "Palette" == band["colorInterpretation"]
       end
 
+      projection = Projection.new(@path)
+      args = ["-projwin", *@map.projwin(projection), @path, crop_path]
+      args += ["-expand", "rgba", *args] if palette
+      OS.gdal_translate *args
+
       return Numeric === @resolution ? @resolution : @map.get_raster_resolution(crop_path), crop_path
+    rescue OS::Error
+      raise "invalid raster file: #{@path}"
     end
   end
 end

f28328b319627e6b6f80e33f4f0b9b446ebf3547
Matthew Hollingworth <mdholling@gmail.com>
2023-01-22 22:37:28 +1100

    add gamma option for mask layers

--- lib/nswtopo/layer/mask_render.rb
+++ lib/nswtopo/layer/mask_render.rb
@@ -14,16 +14,20 @@ module NSWTopo
         defs.add_element "image", "id" => "#{@name}.content", "transform" => transform, "width" => width, "height" => height, "image-rendering" => "optimizeQuality", "href" => href
       end
 
-      colour, shade = @params.values_at "colour", "shade"
+      colour, shade, gamma = @params.values_at "colour", "shade", "gamma"
       raise "can't specify both colour and shade values" if colour && shade
 
-      defs.add_element("filter", "id" => "#{@name}.filter").tap do |filter|
-        filter.add_element("feColorMatrix", "values" => "-1 0 0 0 1  -1 0 0 0 1  -1 0 0 0 1  0 0 0 1 0", "color-interpolation-filters" => "sRGB")
-      end if shade
+      filter = defs.add_element("filter", "id" => "#{@name}.filter") if shade || gamma
+      filter.add_element("feColorMatrix", "values" => "-1 0 0 0 1  -1 0 0 0 1  -1 0 0 0 1  0 0 0 1 0", "color-interpolation-filters" => "sRGB") if shade
+      filter.add_element("feComponentTransfer", "color-interpolation-filters" => "linearRGB").tap do |transfer|
+        transfer.add_element("feFuncR", "type" => "gamma", "exponent" => gamma)
+        transfer.add_element("feFuncG", "type" => "gamma", "exponent" => gamma)
+        transfer.add_element("feFuncB", "type" => "gamma", "exponent" => gamma)
+      end if gamma
 
       defs.add_element("mask", "id" => "#{@name}.mask").tap do |mask|
         use = mask.add_element("use", "href" => "##{@name}.content")
-        use.add_attributes("filter" => "url(##{@name}.filter)") if shade
+        use.add_attributes("filter" => "url(##{@name}.filter)") if filter
 
         cutouts.each.with_object mask.add_element("g", "filter" => "url(#map.filter.cutout)") do |cutout, group|
           group.add_element cutout.use

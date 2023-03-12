aebb3e1621507390c6a4d0e3f9ccaad23b4f7a72
Matthew Hollingworth <mdholling@gmail.com>
2023-03-13 10:37:47 +1100

    tidyup

--- lib/nswtopo/layer/vector.rb
+++ lib/nswtopo/layer/vector.rb
@@ -3,8 +3,36 @@ require_relative 'vector/knockout'
 
 module NSWTopo
   module Vector
-    SVG_ATTRIBUTES = %w[fill-opacity fill font-family font-size font-style font-variant font-weight letter-spacing opacity paint-order stroke-dasharray stroke-dashoffset stroke-linecap stroke-linejoin stroke-miterlimit stroke-opacity stroke-width stroke text-decoration visibility word-spacing nswtopo:overprint]
-    FONT_SCALED_ATTRIBUTES = %w[word-spacing letter-spacing stroke-width line-height]
+    SVG_ATTRIBUTES = %w[
+      fill-opacity
+      fill
+      font-family
+      font-size
+      font-style
+      font-variant
+      font-weight
+      letter-spacing
+      opacity
+      paint-order
+      stroke-dasharray
+      stroke-dashoffset
+      stroke-linecap
+      stroke-linejoin
+      stroke-miterlimit
+      stroke-opacity
+      stroke-width
+      stroke
+      text-decoration
+      visibility
+      word-spacing
+      nswtopo:overprint
+    ]
+    FONT_SCALED_ATTRIBUTES = %w[
+      word-spacing
+      letter-spacing
+      stroke-width
+      line-height
+    ]
     SHIELD_X, SHIELD_Y = 1.0, 0.5
     MARGIN = { mm: 1.0 }
     VALUE, POINT, ANGLE = "%.5f", "%.5f %.5f", "%.2f"

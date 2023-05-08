8bd6259c63dca8194cfa5b161aad67a664d426c6
Matthew Hollingworth <mdholling@gmail.com>
2023-05-08 12:09:07 +1000

    just return a VRT when importing a raster layer

--- lib/nswtopo/layer/raster_import.rb
+++ lib/nswtopo/layer/raster_import.rb
@@ -1,18 +1,15 @@
 module NSWTopo
   module RasterImport
     def get_raster(temp_dir)
-      crop_path = temp_dir / "crop.tif"
       @path = Pathname(@path).expand_path(@source ? @source.parent : Pathname.pwd)
-
-      json = OS.gdalinfo "-json", @path
-      palette = JSON.parse(json)["bands"].any? do |band|
-        "Palette" == band["colorInterpretation"]
+      temp_dir.join("import.vrt").tap do |vrt_path|
+        JSON.parse(OS.gdalinfo "-json", @path).fetch("bands").any? do |band|
+          "Palette" == band["colorInterpretation"]
+        end.then do |palette|
+          args = ["-expand", "rgba"] if palette
+          OS.gdal_translate *args, @path, vrt_path
+        end
       end
-
-      args = ["-expand", "rgba"] if palette
-      OS.gdal_translate *args, @path, crop_path
-
-      return crop_path
     rescue OS::Error
       raise "invalid raster file: #{@path}"
     end

5eee8e679b1d68b70e856193535ec966f544e3a7
Matthew Hollingworth <mdholling@gmail.com>
2023-08-03 10:58:22 +1000

    refactor config command

--- lib/nswtopo/commands/config.rb
+++ lib/nswtopo/commands/config.rb
@@ -1,26 +1,31 @@
 module NSWTopo
   def config(layer = nil, **options)
-    delete, chrome, path, resolution, layer_dir = options.values_at *%i[delete chrome path resolution layer-dir]
-
-    raise "not a directory: %s" % layer_dir if layer_dir && !layer_dir.directory?
-    raise "chrome path is not an executable" if chrome && (chrome.directory? || !chrome.executable?)
-    Config.store("chrome", chrome.to_s) if chrome
-    Config.store("layer-dir", layer_dir.to_s) if layer_dir
-
-    options.slice(*%i[labelling debug gpu versioning zlib-level knockout]).each do |switch, value|
-      Config.store switch.to_s, value
-    end
-
+    path, resolution = options[:path], options[:resolution]
     layer = Layer.sanitise layer
+
     case
     when !layer
       raise OptionParser::InvalidArgument, "no layer name specified for path" if path
       raise OptionParser::InvalidArgument, "no layer name specified for resolution" if resolution
     when path || resolution
-      Config.store(layer, "path", path.to_s) if path
-      Config.store(layer, "resolution", resolution) if resolution
+      Config.store layer, "path", path.to_s if path
+      Config.store layer, "resolution", resolution if resolution
     end
-    Config.delete(*layer, delete) if delete
+
+    options.each do |key, value|
+      case key
+      when :chrome
+        raise "chrome path is not an executable" unless value.executable? && !value.directory?
+        Config.store key.to_s, value.to_s
+      when :"layer-dir"
+        raise "not a directory: %s" % value unless value.directory?
+        Config.store key.to_s, value.to_s
+      when *%i[labelling debug gpu versioning zlib-level knockout]
+        Config.store key.to_s, value
+      when :delete
+        Config.delete *layer, value
+      end
+    end
 
     if options.empty?
       puts Config.to_str.each_line.drop(1)
